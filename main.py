fb64c98a8b62e3db7612776b3b210d89dbe94acf
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-08-27 16:38:06 +0300

    formating comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# Создание главной страницы на сервере при вводе /main отобразится заголовок hello
+# Создание главной страницы на сервере 
 @app.route('/')
 def main():
     return render_template('base.html', title='Mathew Bio')
@@ -13,7 +13,7 @@ def main():
 def foto():
     return render_template('bio.html', title='My foto')
 
-@app.route('/secondfoto')  # Создание первой страницы
+@app.route('/secondfoto')  # Создание второй страницы
 def secondfoto():
     return render_template('secondfoto.html', title='My second foto')
 
