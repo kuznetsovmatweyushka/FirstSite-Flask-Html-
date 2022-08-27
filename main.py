6d27172a903d38f9a71656a373aefb1da7f6f4d9
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-08-27 16:53:15 +0300

    shit posting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# Создание главной страницы на сервере 
+# Создание главной страницы на сервере
 @app.route('/')
 def main():
     return render_template('base.html', title='Mathew Bio')
@@ -13,8 +13,15 @@ def main():
 def foto():
     return render_template('bio.html', title='My foto')
 
+
 @app.route('/secondfoto')  # Создание второй страницы
-def secondfoto():
+def secondFoto():
     return render_template('secondfoto.html', title='My second foto')
+
+
+@app.route('/gitinstruction')  # Создание третьей страницы
+def gitInstruction():
+    return render_template('GitInstruction.md', title='My GitInstruction')
+
 
 app.run()
