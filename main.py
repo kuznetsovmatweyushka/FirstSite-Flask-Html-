83e650103b3b78adc74f7e0b5a1722d4c1d05d76
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-08-31 18:29:33 +0300

    add reg+log html file

--- main.py
+++ main.py
@@ -7,28 +7,23 @@ app.config['SECRET_KEY'] = 'world world world hello'  # csrf - atacks
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    form = LoginForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        surname = form.surname.data
-        email = form.email.data
-        print(name, surname, email)
-        return render_template('base.html', title='Главная страница',
-                               message='Вы авторизовались!')
-    return render_template('base.html', title='Главная страница',
-                           form=form)
+    return render_template('base.html', title='Главная страница')
 
-@app.route('/bio')  # Создание первой страницы
-def foto():
-    return render_template('bio.html', title='My foto')
 
-@app.route('/secondfoto')  # Создание второй страницы
-def secondFoto():
-    return render_template('secondfoto.html', title='My second foto')
+@app.route('/login', methods=['GET', 'POST'])  # Создание первой страницы
+def login():
+    return render_template('login.html', title='Мой профиль')
+
+
+@app.route('/reg', methods=['GET', 'POST'])  # Создание второй страницы
+def registration():
+    return render_template('reg.html', title='Регистрация')
+
 
 @app.route('/gitinstruction')  # Создание третьей страницы
 def gitInstruction():
     return render_template('GitInstruction.md', title='My GitInstruction')
+
 
 if __name__ == '__main__':
     app.run()
